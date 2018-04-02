e2083c844c89d396da2e5a9a361c2651d649e333
Your Name <williamputnamjr@gmail.com>
2018-04-02 12:16:33 -0700

    created basic express server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, function () {
+    console.log("Server is running on " + port + " port");
+});
+
 
 

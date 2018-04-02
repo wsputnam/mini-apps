4ab6bdbe946b457c89d2771375734b699730a9cf
Your Name <williamputnamjr@gmail.com>
2018-04-02 16:41:53 -0700

    data sent to server properly

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,18 @@
 
 
+$('form').on('submit', function (e) {
+    e.preventDefault();
+    var data = {item: e.target[0].value};
+    fetch('button', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (err) {
+        // Error :(
+    });
+});
+
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('<h1>Hello World!</h1>')
+app.use(express.static('client'));
+
+// body parser
+app.use(function (req, res, next) {
+    var data = "";
+    req.on('data', function (chunk) { data += chunk })
+    req.on('end', function () {
+        req.body = JSON.parse(data);
+        next();
+    })
 })
+
+app.use(express.static('./app.js'));
+
+app.get('*', function (req, res) {
+    res.send('index.html');
+});
+
+app.post('*', function (req, res) {
+    console.log(req.body.item)
+    res.send(req.body.item);
+});
+
+
 
 app.listen(port, function () {
     console.log("Server is running on " + port + " port");

aecd9ee13173d884130a3ce4a1a459f56cadcd12
Your Name <williamputnamjr@gmail.com>
2018-05-23 11:38:14 -0700

    starting connect four set up server and react app

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			buttons: []
+		}
+	}
 
+	render() {
+		return (
+          <div>Hello</div>
+          <Row buttons={this.state.buttons}/>
+
+			)
+	}
+}
+
+class Row extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+          {this.props.buttons.map((button) => {
+          	return <Button />;
+          })}
+          <div>line></div>
+			)
+	}
+}
+
+class Button extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+          <div>Button</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public')
+app.use(bodyParser.json())
 
 
